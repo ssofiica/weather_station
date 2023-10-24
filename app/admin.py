beef9b8b87a770756bfce1bb1a02581dbaa6d962
Sofia Valova <91909561+ssofiica@users.noreply.github.com>
2023-10-24 20:13:32 +0300

    Delete app directory

--- app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.Phenomens)
-admin.site.register(models.Users)
-admin.site.register(models.Request)
-admin.site.register(models.PhenomRecord)
